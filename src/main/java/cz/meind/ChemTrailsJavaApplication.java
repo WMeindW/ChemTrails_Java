f7d6fbcc985e873d5fa06098721edd5ce37e95df
Meind <105383135+WMeindW@users.noreply.github.com>
2023-09-12 16:55:33 +0200

    Add files via upload

---
+++ src/main/java/cz/meind/ChemTrailsJavaApplication.java
@@ -0,0 +1,13 @@
+package cz.meind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemTrailsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChemTrailsJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/meind/ChemTrailsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.meind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemTrailsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
